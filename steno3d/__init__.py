860411ec332d4a38884ea4c848d2ed3f95a30ddd
Franklin Koch <franklin@3ptscience.com>
2016-07-12 15:54:36 -0600

    Bump version: 0.1.5 → 0.1.6

--- steno3d/__init__.py
+++ steno3d/__init__.py
@@ -37,7 +37,7 @@ from .surface import *
 from .texture import *
 from .volume import *
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __author__ = '3point Science, Inc.'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 3point Science, Inc.'
--- steno3d/client.py
+++ steno3d/client.py
@@ -20,7 +20,7 @@ from six.moves.urllib.parse import urlparse
 from .user import User
 
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 PRODUCTION_BASE_URL = 'https://steno3d.com/'
 API_SUBPATH = 'api/'

52f68ac704d08923acf542f11908838092e54f2a
Franklin Koch <franklin@3ptscience.com>
2016-07-20 12:03:47 -0600

    Bump version: 0.1.7 → 0.1.8

--- steno3d/client.py
+++ steno3d/client.py
@@ -20,7 +20,7 @@ from six.moves.urllib.parse import urlparse
 from .user import User
 
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 PRODUCTION_BASE_URL = 'https://steno3d.com/'
 API_SUBPATH = 'api/'

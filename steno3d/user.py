96dc55893ffd536339d48935a2d924e2ce29ee3c
Franklin Koch <franklin@3ptscience.com>
2016-07-21 16:45:50 -0600

    Clean up user

--- steno3d/user.py
+++ steno3d/user.py
@@ -35,12 +35,10 @@ class User(properties.PropertyClass):
         default=25
     )
 
-
     def _on_prop_change(self, prop, pre, post):
         if not pre == post:
             print('User data must be modified in settings at steno3d.com')
             setattr(self, '_p_' + prop, pre)
-
 
     def login_with_json(self, login_json):
         setattr(self, '_p_username', login_json['uid'])
@@ -62,31 +60,6 @@ class User(properties.PropertyClass):
         setattr(self, '_p_location', None)
         setattr(self, '_p_devel_key', None)
 
-
     @property
     def logged_in(self):
         return self.username is not None
-
-
-
-    def get_user(self):
-        if getattr(self, '_me', None) is not None:
-            return self._me
-        elif getattr(self, '_user', None) is None:
-            return None
-        else:
-            username = self._user['uid']
-            email = self._user['email']
-            name = self._user['name']
-            url = self._user['url']
-            affiliation = self._user['affiliation']
-            location = self._user['location']
-            self._me = User(
-                username=username if username is not None else 'None',
-                email=email if email is not None else 'None',
-                name=name if name is not None else 'None',
-                url=url if url is not None else 'None',
-                affiliation=affiliation if affiliation is not None else 'None',
-                location=location if location is not None else 'None',
-            )
-            return self._me

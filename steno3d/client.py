1848f1c86ab1b7365c3ea41eee4fc928085feaf5
fwkoch <franklin@3ptscience.com>
2016-11-25 17:17:59 -0700

    Bump version: 0.2.13 → 0.2.14b0

--- steno3d/client.py
+++ steno3d/client.py
@@ -21,7 +21,7 @@ from six.moves.urllib.parse import urlparse
 from .user import User
 
 
-__version__ = '0.2.13'
+__version__ = '0.2.14b0'
 
 PRODUCTION_BASE_URL = 'https://steno3d.com/'
 SLEEP_TIME = .75

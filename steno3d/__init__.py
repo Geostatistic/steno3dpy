97c76151dc499f039d15944cb1cc80130e3c7c8c
Franklin Koch <franklinwkoch@gmail.com>
2017-02-20 15:58:49 +1300

    Bump version: 0.2.14b0 → 0.2.14

--- steno3d/__init__.py
+++ steno3d/__init__.py
@@ -38,7 +38,7 @@ from .texture import *
 from .vector import *
 from .volume import *
 
-__version__ = '0.2.14b0'
+__version__ = '0.2.14'
 __author__ = '3point Science'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 3point Science'

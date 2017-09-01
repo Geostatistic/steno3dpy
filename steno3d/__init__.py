c1ede76f79c3a41d28af3ed95d30975fd5efde13
Franklin Koch <franklinwkoch@gmail.com>
2017-09-01 16:58:50 -0600

    Bump version: 0.3.2 → 0.3.3

--- steno3d/__init__.py
+++ steno3d/__init__.py
@@ -46,7 +46,7 @@ from .texture import *
 from .vector import *
 from .volume import *
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 __author__ = 'ARANZ Geo Limited'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 ARANZ Geo Limited'
--- steno3d/client.py
+++ steno3d/client.py
@@ -20,7 +20,7 @@ from six.moves.urllib.parse import urlparse
 from .user import User
 
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 PRODUCTION_BASE_URL = 'https://steno3d.com/'
 SLEEP_TIME = .75
